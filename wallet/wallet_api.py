1a6706e0e21426eef319050974f27ec7dd147f49
ahmedalaa1212 <Ahmedpop3ahmed@gmail.com>
2026-09-05 19:46:10 +0300

    Update wallet_api.py

--- wallet/wallet_api.py
+++ wallet/wallet_api.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify, request
+from flask import Blueprint, jsonify, request, make_response
 from .wallet_db import get_user_wallet_balances
 
 # ربط واجهات برمجة التطبيقات (APIs) للقوائم الفرعية الثلاث
@@ -15,7 +15,7 @@ wallet_bp.register_blueprint(withdraw_bp, url_prefix='/withdraw')
 
 @wallet_bp.route('/data', methods=['GET'])
 def get_wallet_data():
-    """جلب أرصدة ZN و USDT و ZNX مع التحقق الأمني المزدوج"""
+    """جلب أرصدة ZN و USDT و ZNX مع التحقق الأمني وتحديث لحظي بدون كاش"""
     user_id = request.args.get('user_id', type=int)
     header_user_id = request.headers.get('X-Telegram-User-Id')
     
@@ -26,9 +26,17 @@ def get_wallet_data():
         return jsonify({'success': False, 'error': 'معرف المستخدم غير متاح'}), 400
         
     balances = get_user_wallet_balances(user_id)
-    return jsonify({
+    
+    response = make_response(jsonify({
         'success': True,
         'zn_balance': balances.get('zn_balance', 0.0),
         'usdt_balance': balances.get('usdt_balance', 0.0),
         'znx_balance': balances.get('znx_balance', 0.0)
-    })
+    }))
+    
+    # منع التخزين المؤقت الكاش نهائياً لضمان قراءة الفيربيس اللحظية
+    response.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate, max-age=0'
+    response.headers['Pragma'] = 'no-cache'
+    response.headers['Expires'] = '0'
+    
+    return response
